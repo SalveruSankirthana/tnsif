a4a46d5608d86081b4d3095624c8c246aeab667f
salve <salve@lavanya>
2023-08-16 22:24:56 +0530

    4 commit

--- src/com/tnsif/sub_pack/D.java
+++ src/com/tnsif/sub_pack/D.java
@@ -1,7 +1,8 @@
 package com.tnsif.sub_pack;
 
 public class D {
-public void msg() {
-	System.out.println("Hello from class D");
-}
+	public void msg() {
+		System.out.println("Hello from class D");
+	}
+
 }
