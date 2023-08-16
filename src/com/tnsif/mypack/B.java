48f96f9140f88870a6347f808729ad53ad0db994
salve <salve@lavanya>
2023-08-16 22:24:03 +0530

    3 commit

--- src/com/tnsif/mypack/B.java
+++ src/com/tnsif/mypack/B.java
@@ -1,13 +1,15 @@
 package com.tnsif.mypack;
+
 public class B {
 
 	public static void main(String[] args) {
 		com.tnsif.pack.A obj = new com.tnsif.pack.A();
 		com.tnsif.pack.C obj1 = new com.tnsif.pack.C();
-		com.tnsif.sub_pack.D obj2 = new com.tnsif.sub_pack.D();		
-		obj.msg();
-		obj1.msg();
+		com.tnsif.sub_pack.D obj2= new com.tnsif.sub_pack.D();
+        obj.msg();
+        obj1.msg();
         obj2.msg();
+
 	}
 
 }
